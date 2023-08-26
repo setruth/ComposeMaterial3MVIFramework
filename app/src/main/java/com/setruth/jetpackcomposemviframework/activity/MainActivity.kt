60972b182b04bbf4a6ab6e89ac5e4afc2ce09200
setruth <1607908758@qq.com>
2023-08-26 14:10:12 +0800

    compose material3 MVI架构搭建 第一次提交 实现简单的页面跳转和分包 引入hilt框架作为注入

---
+++ app/src/main/java/com/setruth/jetpackcomposemviframework/activity/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.setruth.jetpackcomposemviframework.activity
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.setruth.jetpackcomposemviframework.constant.APPRoute
+import com.setruth.jetpackcomposemviframework.ui.screen.login.LoginView
+import com.setruth.jetpackcomposemviframework.ui.screen.main.MainView
+import com.setruth.jetpackcomposemviframework.ui.theme.JetpackComposeMVIFrameworkTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeMVIFrameworkTheme {
+                val appNavController = rememberNavController()
+                NavHost(navController = appNavController, startDestination = APPRoute.LOGIN) {
+                    composable(APPRoute.LOGIN) {
+                        LoginView(appNavController)
+                    }
+                    composable(APPRoute.MAIN_VIEW) {
+                        MainView(appNavController)
+                    }
+                }
+            }
+        }
+    }
+}
